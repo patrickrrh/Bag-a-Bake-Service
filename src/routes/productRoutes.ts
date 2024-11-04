94c3eaad7400d363939cb4cdc4c954ec4a0cd079
Nelsen-G <nelsengabriel@gmail.com>
2024-11-04 23:12:28 +0700

    List Product, Create Product, Edit Product

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -13,5 +13,6 @@ router.delete("/delete/product/id", productController.deleteProductById);
 router.post("/get/products/category", productController.getProductsByCategory);
 router.post("/get/recommended/products", productController.findRecommendedProducts);
 router.get("/get/expiring/products", productController.findExpiringProducts);
+router.post("/get/products/bakery", productController.getProductsByBakeryId);
 
 export default router
