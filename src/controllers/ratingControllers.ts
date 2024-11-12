99e3c5e46f81a65798a0034e1407b026a8405eaf
Patrick R.H. <patrickrh31@gmail.com>
2024-11-12 21:08:14 +0700

    Added rating for order

--- src/controllers/ratingControllers.ts
+++ src/controllers/ratingControllers.ts
@@ -6,15 +6,15 @@ const ratingServices = new RatingServices();
 export class RatingController {
     public async createRating(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const { userId, bakeryId, rating } = req.body;
+            const { orderId, rating, review } = req.body;
     
-            if (!userId || !bakeryId || !rating) {
-                console.log("[src][controllers][RatingController][createRating] userId, bakeryId or rating is required");
-                res.status(400).send("userId, bakeryId or rating is required");
+            if (!orderId || !rating) {
+                console.log("[src][controllers][RatingController][createRating] orderId or rating is required");
+                res.status(400).send("orderId or rating is required");
                 return;
             }
 
-            const createdRating = await ratingServices.createRating({ userId, bakeryId, rating });
+            const createdRating = await ratingServices.createRating({ orderId, rating, review });
             res.status(201).json({
                 status: 201,
                 data: createdRating
